2762c68460e30cf959f1bdc9d8c3ff47c9d59cea
alexander.zonov <alexander.zonov@developonbo.ru>
2018-03-07 17:24:01 +0300

    Clean-up code

--- src/errors.js
+++ src/errors.js
@@ -2,7 +2,7 @@ const assert = require('assert-plus');
 const { LDAPResult } = require('./messages');
 const CODES = require('./utils/error-codes');
 
-const ERRORS = [];
+const ERRORS = {};
 const capitalize = str => str.charAt(0) + str.slice(1).toLowerCase();
 
 class LDAPError extends Error {
@@ -43,16 +43,6 @@ class ConnectionError extends LDAPError {
   }
 }
 
-class AbandonedError extends LDAPError {
-  constructor(message) {
-    super(message, null, AbandonedError);
-  }
-
-  get name() {
-    return 'AbandonedError';
-  }
-}
-
 class TimeoutError extends LDAPError {
   constructor(message) {
     super(message, null, TimeoutError);
@@ -63,57 +53,39 @@ class TimeoutError extends LDAPError {
   }
 }
 
+Object.keys(CODES)
+  .filter(key => key !== 'LDAP_SUCCESS')
+  .forEach(key => {
+    const pieces = key.split('_').slice(1).map(capitalize);
+    if (pieces[pieces.length - 1] !== 'Error') {
+      pieces.push('Error');
+    }
+
+    ERRORS[CODES[key]] = class extends LDAPError {
+      get message() {
+        return pieces.join(' ');
+      }
+
+      get name() {
+        return pieces.join('');
+      }
+
+      get code() {
+        return CODES[key];
+      }
+    };
+  });
 
 module.exports = {
-  LDAPError,
   ConnectionError,
-  AbandonedError,
   TimeoutError,
+  ProtocolError: ERRORS[CODES.LDAP_PROTOCOL_ERROR],
 
-  getMessage(code) {
-    assert.number(code, 'code (number) required');
-
-    const errObj = ERRORS[code];
-    return errObj && errObj.message ? errObj.message : '';
-  },
+  LDAP_SUCCESS: CODES.LDAP_SUCCESS,
 
   getError(res) {
     assert.ok(res instanceof LDAPResult, 'res (LDAPResult) required');
 
-    const errObj = ERRORS[res.status];
-    const E = module.exports[errObj.err];
-    return new E(res.errorMessage || errObj.message, res.matchedDN || null, module.exports.getError);
+    return new (ERRORS[res.status])(null, res.matchedDN || null, module.exports.getError);
   }
 };
-
-// Some whacky games here to make sure all the codes are exported
-Object.keys(CODES).forEach(code => {
-  module.exports[code] = CODES[code];
-
-  if (code === 'LDAP_SUCCESS')
-    return;
-
-  const pieces = code.split('_').slice(1).map(capitalize);
-  if (pieces[pieces.length - 1] !== 'Error') {
-    pieces.push('Error');
-  }
-
-  const err = pieces.join(''); // At this point LDAP_OPERATIONS_ERROR is now OperationsError
-  const message = pieces.join(' '); // and 'Operations Error'
-
-  ERRORS[CODES[code]] = { err, message };
-
-  module.exports[err] = class extends LDAPError {
-    constructor(message, dn, caller) {
-      super(message, dn, caller || module.exports[err]);
-    }
-
-    get name() {
-      return err;
-    }
-
-    get code() {
-      return CODES[code];
-    }
-  };
-});

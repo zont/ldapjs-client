313ef16ae87699033c7d29564cc88eb0324b79d0
Vasya-Chajko <Vasya.Chajko@Gmail.com>
2025-04-15 09:32:11 +0700

    Update controlToBer.js

--- src/controls/controlToBer.js
+++ src/controls/controlToBer.js
@@ -3,22 +3,22 @@ const asn1 = require('asn1')
 const BerWriter = asn1.BerWriter;
 
 module.exports = (control, writer) => {
-    writer.startSequence();
-    writer.writeString(control.OID);
-    writer.writeBoolean(control.criticality);
+  writer.startSequence();
+  writer.writeString(control.OID);
+  writer.writeBoolean(control.criticality);
 
-    if (control.OID === OID.PagedResults) {
-        const ber = new BerWriter();
-        ber.startSequence();
-        ber.writeInt(control.value.size);
-        if ( control.value.cookie === '' ) {
-            ber.writeString('');
-        } else {
-            ber.writeBuffer(control.value.cookie, asn1.Ber.OctetString);
-        }
-        ber.endSequence();
-        writer.writeBuffer(ber.buffer, 0x04);
-	}
+  if (control.OID === OID.PagedResults) {
+    const ber = new BerWriter();
+    ber.startSequence();
+    ber.writeInt(control.value.size);
+    if ( control.value.cookie === '' ) {
+      ber.writeString('');
+    } else {
+      ber.writeBuffer(control.value.cookie, asn1.Ber.OctetString);
+    }
+    ber.endSequence();
+    writer.writeBuffer(ber.buffer, 0x04);
+  }
 	
-	writer.endSequence();
+  writer.endSequence();
 }

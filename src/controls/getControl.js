928e1dd0e2e1a7f3b925aa91865d8c1e1b2bc00d
Vasya-Chajko <Vasya.Chajko@Gmail.com>
2025-04-15 09:50:25 +0700

    Update getControl.js

--- src/controls/getControl.js
+++ src/controls/getControl.js
@@ -4,36 +4,36 @@ const BerReader = asn1.BerReader;
 const OID = require('./OID');
 
 module.exports = (ber) => {
-	if (ber.readSequence() === null) { return null; }
+  if (ber.readSequence() === null) { return null; }
 
-    const control = {
-      OID: '',
-      criticality: false,
-      value: null
+  const control = {
+    OID: '',
+    criticality: false,
+    value: null
+  }
+
+  if (ber.length) {
+    const end = ber.offset + ber.length;
+
+    control.OID = ber.readString();
+    if (ber.offset < end) {
+      if (ber.peek() === Ber.Boolean) { control.criticality = ber.readBoolean(); }
     }
 
-    if (ber.length) {
-      const end = ber.offset + ber.length;
+    if (ber.offset < end) { control.value = ber.readString(Ber.OctetString, true); }
 
-      control.OID = ber.readString();
-      if (ber.offset < end) {
-        if (ber.peek() === Ber.Boolean) { control.criticality = ber.readBoolean(); }
+    if (control.OID === OID.PagedResults) {
+      const ber = new BerReader(control.value);
+      if (ber.readSequence()) {
+        control.value = {};
+        control.value.size = ber.readInt();
+        control.value.cookie = ber.readString(asn1.Ber.OctetString, true);
+        if ( control.value.cookie.length === 0 ) {
+          control.value.cookie = '';
+        }
       }
+    }
+  }
 
-		if (ber.offset < end) { control.value = ber.readString(Ber.OctetString, true); }
-	  
-		if (control.OID === OID.PagedResults) {
-			const ber = new BerReader(control.value);
-			if (ber.readSequence()) {
-				control.value = {};
-				control.value.size = ber.readInt();
-				control.value.cookie = ber.readString(asn1.Ber.OctetString, true);
-				if ( control.value.cookie.length === 0 ) {
-					control.value.cookie = '';
-				}
-			}
-		}
-	}
-
-	return control;
+  return control;
 }
